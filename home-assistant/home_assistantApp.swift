b4cea80c27a05e70036fb0bc30a5f24da9974e55
dingyuqing <dingyuqing@shizhuang-inc.com>
2025-06-12 20:55:32 +0800

    Initial Commit

---
+++ home-assistant/home_assistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  home_assistantApp.swift
+//  home-assistant
+//
+//  Created by admin on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct home_assistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
